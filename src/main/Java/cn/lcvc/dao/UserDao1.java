d147998960c369257c1c99772e072d5070725edb
aq846908158 <609723911@qq.com>
2017-11-29 17:36:23 +0800

    添加了UserDao1接口

---
+++ src/main/Java/cn/lcvc/dao/UserDao1.java
@@ -0,0 +1,39 @@
+package cn.lcvc.dao;
+
+import java.util.List;
+
+public interface UserDao1 {
+
+    public void addUser();
+    public void deleteUser();
+    public void updateUser();
+    public Object getUser(Integer id);
+    public List<Object> getUserList();
+    /**
+     * @author huanghaibin
+     * @date 2017-11-29 16:35:15
+     * @param_column需要查询的字段
+     * @param_value需要查询的字段的值
+     * @return 一个User对象，null表示没查到
+     */
+    public Object getUserBy_OneColumn(String column,Object value);
+    /**
+     * @author huanghaibin
+     * @date 2017-11-29 16:59:53
+     * @param_ column1需要查询的第一个字段
+     * @param_ value1需要查询的第一个字段的值
+     * @param_ column1需要查询的第二个字段
+     * @param_ value1需要查询的第二个字段的值
+     * @return 一个User对象，null表示没查到
+     */
+    public Object getUserBy_TowColumn(String column1,Object value1,String column2,Object value2);
+
+    /**
+     * @author huanghaibin
+     * @date 2017-11-29 17:21:59
+     * @param_column排序所依据的字段
+     * @param_orderBy排序规则 如"asc","desc"
+     * @return 一个有序List<User>集合，null表示没查到
+     */
+    public List<Object> getUserListOrderBy(String column,String orderBy);
+}
